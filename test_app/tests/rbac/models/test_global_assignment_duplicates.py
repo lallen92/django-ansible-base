cbae184d1caf2564b642c924116d84b44d7e9059
Liam Allen <lallen@redhat.com>
2026-08-04 16:50:46 +0100

    Removing tests, feel excessive just to please codecov

--- test_app/tests/rbac/models/test_global_assignment_duplicates.py
+++ test_app/tests/rbac/models/test_global_assignment_duplicates.py
@@ -1,10 +1,9 @@
 """Regression tests for AAP-83436: duplicate global role assignments."""
 
 import pytest
-from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 
-from ansible_base.rbac.models import RoleDefinition, RoleTeamAssignment, RoleUserAssignment
+from ansible_base.rbac.models import RoleTeamAssignment, RoleUserAssignment
 
 
 @pytest.mark.django_db
@@ -19,25 +18,6 @@ class TestGlobalUserAssignmentConstraint:
         global_inv_rd.give_global_permission(rando)
         with pytest.raises(IntegrityError):
             RoleUserAssignment.objects.create(user=rando, role_definition=global_inv_rd, object_role=None)
-
-    def test_different_users_same_role(self, global_inv_rd):
-        user1 = get_user_model().objects.create(username='user1')
-        user2 = get_user_model().objects.create(username='user2')
-        assignment1 = global_inv_rd.give_global_permission(user1)
-        assignment2 = global_inv_rd.give_global_permission(user2)
-        assert assignment1.pk != assignment2.pk
-
-    def test_same_user_different_roles(self, rando):
-        rd1 = RoleDefinition.objects.create_from_permissions(permissions=['view_inventory'], name='global-view-inv', content_type=None)
-        rd2 = RoleDefinition.objects.create_from_permissions(permissions=['change_inventory', 'view_inventory'], name='global-change-inv', content_type=None)
-        assignment1 = rd1.give_global_permission(rando)
-        assignment2 = rd2.give_global_permission(rando)
-        assert assignment1.pk != assignment2.pk
-        assert RoleUserAssignment.objects.filter(user=rando, object_role__isnull=True).count() == 2
-
-    def test_constraint_does_not_affect_object_assignments(self, rando, inventory, inv_rd):
-        inv_rd.give_permission(rando, inventory)
-        assert RoleUserAssignment.objects.filter(user=rando, role_definition=inv_rd, object_role__isnull=False).count() == 1
 
 
 @pytest.mark.django_db

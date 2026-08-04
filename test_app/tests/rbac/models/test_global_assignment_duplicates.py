794a660af1940dc7bcf1cd4efa6264d11c08e8c9
Liam Allen <lallen@redhat.com>
2026-08-04 15:53:47 +0100

    Test refactor

--- test_app/tests/rbac/models/test_global_assignment_duplicates.py
+++ test_app/tests/rbac/models/test_global_assignment_duplicates.py
@@ -4,7 +4,7 @@ import pytest
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 
-from ansible_base.rbac.models import RoleDefinition, RoleTeamAssignment, RoleUserAssignment, DABContentType
+from ansible_base.rbac.models import RoleDefinition, RoleTeamAssignment, RoleUserAssignment
 
 
 @pytest.mark.django_db
@@ -35,17 +35,9 @@ class TestGlobalUserAssignmentConstraint:
         assert assignment1.pk != assignment2.pk
         assert RoleUserAssignment.objects.filter(user=rando, object_role__isnull=True).count() == 2
 
-
-    def test_constraint_does_not_affect_object_assignments(self, rando, inventory, global_inv_rd):
-        rd = RoleDefinition.objects.create_from_permissions(
-            permissions=['view_inventory'], name='object-view-inv', content_type=global_inv_rd.content_type or RoleUserAssignment.objects.model._meta.app_config.get_model('dabcontenttype').objects.first(),
-        )
-        # This test needs a role definition with a content_type to create object-level assignments
-        ct = DABContentType.objects.get_for_model(inventory)
-        rd = RoleDefinition.objects.create_from_permissions(permissions=['view_inventory'], name='obj-inv-rd', content_type=ct)
-        rd.give_permission(rando, inventory)
-        rd.give_global_permission(rando)
-        assert RoleUserAssignment.objects.filter(user=rando, role_definition=rd).count() == 2
+    def test_constraint_does_not_affect_object_assignments(self, rando, inventory, inv_rd):
+        inv_rd.give_permission(rando, inventory)
+        assert RoleUserAssignment.objects.filter(user=rando, role_definition=inv_rd, object_role__isnull=False).count() == 1
 
 
 @pytest.mark.django_db
